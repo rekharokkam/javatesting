c30b3535124971c0460cb66bff6ae70bf8205e3c
Rekha Rokkam <rekha.rokkam@yahoo.com>
2017-03-30 14:55:49 -0700

    trivial changes

--- src/com/wine/Base64StringGenerator.java
+++ src/com/wine/Base64StringGenerator.java
@@ -8,9 +8,15 @@ public class Base64StringGenerator {
 	{
 		return new String (Base64.getEncoder().encodeToString(toBeEncoded.getBytes()));
 	}
+	
+	public static String getBase64Decoded (String toBeDecoded)
+	{
+		return new String (Base64.getDecoder().decode(toBeDecoded));
+	}
 
 	public static void main(String[] args) 
 	{
-System.out.println("Base 64 encoded string : " + getBase64Encoded("user1:secret"));		
+//System.out.println("Base 64 encoded string : " + getBase64Encoded("user1:secret"));
+System.out.println("Base 64 decoded string : " + getBase64Decoded("Y21zOnMzY3JlVA=="));		
 	}
 }

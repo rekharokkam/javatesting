76f0837af0ce91e11cf5f462cac3b55c10d34f0e
Rekha Rokkam <rekha.rokkam@yahoo.com>
2017-08-21 11:00:55 -0700

    One missed file

--- src/com/wine/Base64StringGenerator.java
+++ src/com/wine/Base64StringGenerator.java
@@ -1,5 +1,7 @@
 package com.wine;
 
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Base64;
 
 public class Base64StringGenerator {
@@ -14,9 +16,11 @@ public class Base64StringGenerator {
 		return new String (Base64.getDecoder().decode(toBeDecoded));
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
 //System.out.println("Base 64 encoded string : " + getBase64Encoded("user1:secret"));
-System.out.println("Base 64 decoded string : " + getBase64Decoded("Y21zOnMzY3JlVA=="));		
+		System.out.println("URL Encoded : " + URLEncoder.encode("http://www.example.com/CEREC® Materials & Accessories/IPS Empress® CAD.pdf", "UTF-8"));
+		System.out.println("Base 64 encoded string : " + getBase64Encoded("14 hands"));
+//System.out.println("Base 64 decoded string : " + getBase64Decoded("Y21zOnMzY3JlVA=="));		
 	}
 }
